8f381298c8e3bdd3742bf5482d9148b330ce5e71
David Hernandez <76022084+davidh167@users.noreply.github.com>
2023-06-07 11:39:54 -0700

    Update the CSV writer

--- JavaEmotivClient/PADSocket.java
+++ JavaEmotivClient/PADSocket.java
@@ -23,8 +23,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
 
 public class PADSocket extends EmotivSocket{
+
+    private int messageCount = 0;
+    List<String[]> temp  = new ArrayList<>();
+    String fileSuffix = String.valueOf(LocalDateTime.now()).replaceAll(":", "-").replaceAll("\\.", "_");
 
 
     public PADSocket(URI serverURI, EnhancedDelegate delegate) throws Exception {
@@ -36,6 +44,7 @@ public class PADSocket extends EmotivSocket{
     public void onMessage(String message) {
 
         System.out.println("Received message from Emotiv server.");
+        System.out.println(fileSuffix);
         if (!delegate.isSubscribed()) {
             JSONObject response = new JSONObject(message);
             int id = response.getInt("id");
@@ -81,8 +90,43 @@ public class PADSocket extends EmotivSocket{
             // Formatted Vector ^
 
             // Calculate PAD value in separate class
+            System.out.println(temp2);
             PadVector vector = new PadVector(temp2);
             System.out.println(vector);
+
+
+            // CSV writer code
+//            messageCount++;
+//            if(messageCount == 7){
+                String[] list = new String[temp2.size()];
+                for(int i = 0; i < temp2.size(); i++){
+                    list[i] = temp2.get(i);
+                }
+
+//                temp.add(list);
+
+                try{
+                    File file = new File("./data/subject_" + fileSuffix + ".csv");
+                    FileWriter outputfile = new FileWriter(file, true);
+                    CSVWriter writer = new CSVWriter(outputfile);
+//                    writer.writeAll(temp);
+                    writer.writeNext(list);
+                    writer.close();
+                    System.out.println("Wrote a line");
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                // Write to file
+//            }else if(messageCount < 7) {
+//                // simply keep adding to array
+//                String[] list = new String[temp2.size()];
+//                for (int i = 0; i < temp2.size(); i++) {
+//                    list[i] = temp2.get(i);
+//                }
+//                temp.add(list);
+//            }
+            // End csv writer code
 
 
 
